808770804e5036ac6a19e8531ceec52c45f431df
Renan Neves <renanlneves@gmail.com>
2022-12-01 18:25:13 -0300

    criadas rotas para deletar pacotes

--- freight/routes/package.py
+++ freight/routes/package.py
@@ -1,19 +1,42 @@
 from asyncio import events
 from fastapi import APIRouter, Body, HTTPException,status
-from typing import Dict
+from typing import List
 from models.package import Package
 
 package_router = APIRouter(tags=["Pacotes"])
 
-packages = {}
+packages = []
 
-@package_router.get("/", response_model= Dict[Package])
-async def retorna_todos_pacotes() -> dict[Package]:
+
+@package_router.get("/", response_model= List[Package])
+async def get_all_packages() -> dict[Package]:
     return packages
-    
+
+
 @package_router.get("/{id}", response_model=Package)
-async def retorna_um_pacote(id: int) -> Package:
+async def get_one_package(id: int) -> Package:
     for package in packages:
         if package.id == id:
             return package
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Pacote com o ID informado não existe.")
+
+
+@package_router.post("/package")
+async def new_package(body: Package = Body(...)) -> dict:
+    packages.append(body)
+    return {"mesagem": "pacote registrado com sucesso."}
+
+
+@package_router.delete("/{id}")
+async def delete_package(id:int) -> dict:
+    for package in packages:
+        if package.id == id:
+            packages.remove(package)
+            return {"mensagem": "Pacote deletado com sucesso!"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="O pacote com a ID informada não existe.")
+
+
+@package_router.delete("/")
+async def delete_all_packages() -> dict:
+    packages.clear()
+    return {"mensagem": "Todos os pacotes foram deletados."}
