096a9af4439b64016b9282b774e0835f3871bf90
Renan Neves <renanlneves@gmail.com>
2022-12-06 09:48:01 -0300

    criando e testando validacoes

--- freight/routes/delivery_type.py
+++ freight/routes/delivery_type.py
@@ -9,11 +9,13 @@ delivery_type = []
 
 
 
-@delivery_router.get("/tipo_entrega")
+@delivery_router.get("/delivery_type")
 async def validar_dimensao():
     for package in packages:
-        if package.dimensao:
-            return package.dimensao["altura"]
+        if package.dimensao["altura"] > 10 and package.dimensao["altura"] < 200:
+            if package.dimensao["largura"] > 6 and package.dimensao["largura"] < 140:
+                if package.peso > 0:
+                    return {"mensagem": "Chegou até aqui!"}
     
 
 
