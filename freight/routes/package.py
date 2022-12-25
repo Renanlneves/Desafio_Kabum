adf9a3d4d03648398b27799c242f7a98d29e0b77
Renanlneves <renanlneves@gmail.com>
2022-12-25 19:08:29 -0300

    organizando codigo

--- freight/routes/package.py
+++ freight/routes/package.py
@@ -12,7 +12,7 @@ packages = []
 delivery_type = []
 
 
-@package_router.post( response_model=List[Delivery_type], status_code=status.HTTP_201_CREATED)
+@package_router.post(response_model=List[Delivery_type], status_code=status.HTTP_201_CREATED)
 async def new_package(pack: Package) -> dict:
     packages.clear()
     delivery_type.clear()
@@ -21,27 +21,28 @@ async def new_package(pack: Package) -> dict:
         if pack.id == package.id:
             raise HTTPException(status_code=status.HTTP_409_CONFLICT,
                                 detail="Um pacote com esse id já existe.")
-    packages.append(pack)
+        packages.append(pack)
 
-    for package in packages:
         entrega_ninja = (package.dimensao["altura"] > 10 and package.dimensao["altura"] < 200) and (
             package.dimensao["largura"] > 6 and package.dimensao["largura"] < 140) and (package.peso > 0)
+
         if entrega_ninja:
-                preco_ninja = round(
-                    (packages[0].peso * freights[0]["constante_calculo"]) / 10, 2)
-                delivery_test = Delivery_type(
-                    nome="Ninja", valor_frete=preco_ninja, prazo_dias=6)
+            preco_ninja = round(
+                (packages[0].peso * freights[0]["constante_calculo"]) / 10, 2)
+            delivery_test = Delivery_type(
+                nome="Ninja", valor_frete=preco_ninja, prazo_dias=6)
 
-                delivery_type.append(delivery_test)
+            delivery_type.append(delivery_test)
 
-        if package.dimensao["altura"] > 5 and package.dimensao["altura"] < 140:
-            if package.dimensao["largura"] > 13 and package.dimensao["largura"] < 125:
-                if package.peso > 0:
-                    preco_kabum = round(
-                        (packages[0].peso * freights[1]["constante_calculo"]) / 10, 2)
-                    delivery_test = Delivery_type(
-                        nome="Kabum", valor_frete=preco_kabum, prazo_dias=6)
+        entrega_kabum = (package.dimensao["altura"] > 5 and package.dimensao["altura"] < 140) and (
+            package.dimensao["largura"] > 13 and package.dimensao["largura"] < 125) and (package.peso > 0)
 
-                    delivery_type.append(delivery_test)
+        if entrega_kabum:
+            preco_kabum = round(
+                (packages[0].peso * freights[1]["constante_calculo"]) / 10, 2)
+            delivery_test = Delivery_type(
+                nome="Kabum", valor_frete=preco_kabum, prazo_dias=6)
+
+            delivery_type.append(delivery_test)
 
     return delivery_type
